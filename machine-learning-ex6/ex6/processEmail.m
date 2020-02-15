2e4d3ac6157e6dac26072a514ca13ad6a9c021e2
jcrecio <juan.carlos.recio.abad@gmail.com>
2020-02-15 14:05:33 +0100

    Added mapping words to indices

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -97,8 +97,11 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
+    for position=1:length(vocabList)
+        if (strcmp(vocabList{position}, str))
+            word_indices = [word_indices position];
+        endif
+    endfor
 
 
 
